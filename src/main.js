ed9d50a4d6c6c78abdba9c904f235e950da9dd61
zhouyy <413993459@qq.com>
2018-04-21 12:09:54 +0800

    vue.js+koa2

---
+++ src/main.js
@@ -0,0 +1,24 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+// 不推荐全局引入vant
+// import Vant from 'vant'
+// import 'vant/lib/vant-css/index.css'
+// Vue.use(Vant)
+
+// 按需引入vant中的组件
+import {Button} from 'vant'
+
+Vue.use(Button)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
